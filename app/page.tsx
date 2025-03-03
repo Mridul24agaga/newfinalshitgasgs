b64710fff1c42497be8f12c05dffa81b2538c26f
Mridul24agaga <mridulthareja8@gmail.com>
2025-03-04 00:52:57 +0530

    ASG

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,17 @@ export default function Page() {
         `}
       </Script>
 
+      {/* Microsoft Clarity Script */}
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "qil10jfr8p");
+        `}
+      </Script>
+
       <AnnouncementBanner />
       <Navbar />
       <div className="px-4 sm:px-6 lg:px-8 py-8">
